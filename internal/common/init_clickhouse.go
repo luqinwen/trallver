93810181c9691bc18bb04ad2d5b81bfd4b9fb76a
jianglei66 <jiangl3024@gmail.com>
2024-07-13 05:45:48 -0700

    Updated init_clickhouse, removed main_service, added clickhouse, and updated log

--- internal/common/init_clickhouse.go
+++ internal/common/init_clickhouse.go
@@ -1,28 +1,28 @@
 package common
 
 import (
-    "database/sql"
-    "log"
+	"database/sql"
+	"log"
 
-    _ "github.com/ClickHouse/clickhouse-go"
+	_ "github.com/ClickHouse/clickhouse-go"
 )
 
 var DB *sql.DB
 
 func InitClickHouse() {
-    var err error
-    DB, err = sql.Open("clickhouse", "tcp://localhost:9000?debug=true")
-    if err != nil {
-        log.Fatalf("Error connecting to ClickHouse: %v", err)
-    }
+	var err error
+	DB, err = sql.Open("clickhouse", "tcp://localhost:9000?debug=true")
+	if err != nil {
+		log.Fatalf("Error connecting to ClickHouse: %v", err)
+	}
 
-    _, err = DB.Exec(`
+	_, err = DB.Exec(`
         CREATE TABLE IF NOT EXISTS my_table (
             timestamp UInt64,
             value     Int32
         ) ENGINE = Log
     `)
-    if err != nil {
-        log.Fatalf("Error creating table: %v", err)
-    }
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
 }
